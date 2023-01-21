32f66f871f6d23d3184a3b2d850116257bb4ad88
François <frsuzeau@gmail.com>
2023-01-21 16:01:53 +0100

    Passing to python 3.11

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 from cx_Freeze import setup, Executable
-base = None
-executables = [Executable("src/main.py", base=base)]
+
+# if your architecture is different then don't hesitate to change the relative path of your file when the main call is
+executables = [
+        Executable(
+            script="src/main.py", 
+            base="Win32GUI",
+            targetName="Wifi Test Manager.exe",
+            icon = None # if you want to use an icon file, specify the file name here
+        )
+]
+
 packages = ["idna"]
+includefiles = [] #include path of your personnal module if they are not located in the same directory of your main script
+binaries = []
+excludes = []
 options = {
     'build_exe': {    
+        'excludes':excludes,
         'packages':packages,
+        'include_files':includefiles,
+        'bin_includes':binaries,
+        "include_msvcr": True
     },
 }
+# all the external module (such as matplot lib etc is copy directly in the lib directory and then transformed in a dynamic library file)
+
+# change the name field with name of your application
 setup(
     name = "Wifi Test Manager",
     options = options,
